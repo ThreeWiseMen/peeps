89bb0f4a7dcd5a9f4ac010779ff71cf3adf83716
Justin Wood <justin.k.wood@gmail.com>
2013-10-25 12:20:05 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peeps::Application.config.secret_key_base = 'e5beff59e178fd7aa9e77bcae981922ad235f1bed80fd47e70cb745586b1424a9764f234ee318a8c5b529b00b23ad0615c800b77c71bddcbb208cd5fcec64641'
