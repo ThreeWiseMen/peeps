8ecb7cc07f99f88daaec04d29cd5b6fca33b01e2
Justin Wood <justin.k.wood@gmail.com>
2013-10-25 12:28:21 -0400

    basic people model with avatar uploader

---
+++ app/models/person.rb
@@ -0,0 +1,8 @@
+class Person < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  mount_uploader :avatar, AvatarUploader
+end
