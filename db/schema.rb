3bf5b9ff3579291bffb0decf1ea4b39671931d86
Justin Wood <justin.k.wood@gmail.com>
2013-10-25 12:55:11 -0400

    Skills model and a basic test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131025162403) do
+ActiveRecord::Schema.define(version: 20131025165009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20131025162403) do
 
   add_index "people", ["email"], name: "index_people_on_email", unique: true, using: :btree
   add_index "people", ["reset_password_token"], name: "index_people_on_reset_password_token", unique: true, using: :btree
+
+  create_table "skills", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "user", force: true do |t|
     t.string "ident",    limit: nil, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Person.delete_all
+Skill.delete_all
 
 justin = Person.create({first_name: 'Justin', last_name: 'Wood',
                          email: 'woodjk@threewisemen.ca', password: 'something'})
+
+cpp = Skill.create(name: 'C++')
+haskell = Skill.create(name: 'Haskell')
