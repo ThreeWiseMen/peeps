62238ccb4dea82cbd538e8c35655e8b061d216a8
Stacey Vetzal <stacey@vetzal.com>
2013-10-28 14:30:38 -0400

    filling out some seeds, fixing spelling on loathe

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,28 @@
 Person.delete_all
 Skill.delete_all
 
-justin = Person.create({first_name: 'Justin', last_name: 'Wood',
-                         email: 'woodjk@threewisemen.ca', password: 'something'})
-
+# Let's set up some skills...
 cpp = Skill.create(name: 'C++')
+php = Skill.create(name: 'PHP')
 haskell = Skill.create(name: 'Haskell')
+ruby = Skill.create(name: 'Ruby')
+java = Skill.create(name: 'Java')
+csharp = Skill.create(name: 'C#')
+clojure = Skill.create(name: 'clojure')
+
+justin = Person.create({first_name: 'Justin', last_name: 'Wood',
+                        email: 'woodjk@threewisemen.ca', password: 'something'})
 
 SkillMapping.create(person: justin, skill: haskell, kind: :learn)
 SkillMapping.create(person: justin, skill: cpp, kind: :love)
+SkillMapping.create(person: justin, skill: php, kind: :loathe)
+
+stacey = Person.create({first_name: 'Stacey', last_name: 'Vetzal',
+                        email: 'stacey@mojility.ca', password: 'something'})
+
+SkillMapping.create(person: stacey, skill: cpp, kind: :love)
+SkillMapping.create(person: stacey, skill: ruby, kind: :love)
+SkillMapping.create(person: stacey, skill: haskell, kind: :learn)
+SkillMapping.create(person: stacey, skill: clojure, kind: :learn)
+SkillMapping.create(person: stacey, skill: java, kind: :loathe)
+SkillMapping.create(person: stacey, skill: csharp, kind: :loathe)
