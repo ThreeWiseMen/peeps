daed5c9b054caeecd8334a495eb644be27bfd8a2
Stacey Vetzal <stacey@vetzal.com>
2013-10-28 16:30:09 -0400

    making skills hierarchial

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,35 @@
 Person.delete_all
 Skill.delete_all
 
-# Let's set up some skills...
-cpp = Skill.create(name: 'C++')
-php = Skill.create(name: 'PHP')
-haskell = Skill.create(name: 'Haskell')
-ruby = Skill.create(name: 'Ruby')
-java = Skill.create(name: 'Java')
-csharp = Skill.create(name: 'C#')
-clojure = Skill.create(name: 'clojure')
+### Let's set up some skills...
+#
+# Languages
+#
+programming = Skill.create(name: 'Programming')
+  cpp = Skill.create(parent: programming, name: 'C++')
+  php = Skill.create(parent: programming, name: 'PHP')
+  haskell = Skill.create(parent: programming, name: 'Haskell')
+  ruby = Skill.create(parent: programming, name: 'Ruby')
+  java = Skill.create(parent: programming, name: 'Java')
+  csharp = Skill.create(parent: programming, name: 'C#')
+  clojure = Skill.create(parent: programming, name: 'Clojure')
+  js = Skill.create(parent: programming, name: 'Javascript')
+  webservices = Skill.create(parent: programming, name: 'Web Services')
+    json = Skill.create(parent: webservices, name: 'JSON')
+    wsdl = Skill.create(parent: webservices,name: "WSDL")
+  xml = Skill.create(parent: programming, name: 'XML') # tags, namespaces, dublin-core, rss, atom
+    xslt = Skill.create(parent: xml, name: 'XSLT')
+    namespaces = Skill.create(parent: xml, name: 'Namespaces')
+      dublincore = Skill.create(parent: namespaces, name: 'Dublin Core')
+      rss = Skill.create(parent: namespaces, name: 'Namespaces')
+      atom = Skill.create(parent: namespaces, name: 'Namespaces')
+#
+# Design
+#
+design = Skill.create(name: 'Design')
+  html5 = Skill.create(parent: design, name: 'HTML5')
+  xhtml = Skill.create(parent: design, name: 'XHTML')
+  css = Skill.create(parent: design, name: 'CSS')
 
 justin = Person.create({first_name: 'Justin', last_name: 'Wood',
                         email: 'woodjk@threewisemen.ca', password: 'something'})
@@ -28,9 +49,10 @@ SkillMapping.create(person: justin, skill: php, kind: :loathe)
 stacey = Person.create({first_name: 'Stacey', last_name: 'Vetzal',
                         email: 'stacey@mojility.ca', password: 'something'})
 
-SkillMapping.create(person: stacey, skill: cpp, kind: :love)
-SkillMapping.create(person: stacey, skill: ruby, kind: :love)
-SkillMapping.create(person: stacey, skill: haskell, kind: :learn)
-SkillMapping.create(person: stacey, skill: clojure, kind: :learn)
-SkillMapping.create(person: stacey, skill: java, kind: :loathe)
-SkillMapping.create(person: stacey, skill: csharp, kind: :loathe)
+SkillMapping.create(person: stacey, skill: programming, kind: :love)
+  SkillMapping.create(person: stacey, skill: cpp, kind: :love)
+  SkillMapping.create(person: stacey, skill: ruby, kind: :love)
+  SkillMapping.create(person: stacey, skill: haskell, kind: :learn)
+  SkillMapping.create(person: stacey, skill: clojure, kind: :learn)
+  SkillMapping.create(person: stacey, skill: java, kind: :loathe)
+  SkillMapping.create(person: stacey, skill: csharp, kind: :loathe)
